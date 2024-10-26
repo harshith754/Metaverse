c895004c1e60d993a288679a8a7a3e716cb8d257
harshith <kunderharshith3@gmail.com>
2024-10-27 00:42:33 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+
+const app = express();
+const PORT = 5000;
+
+// Create an HTTP server with Express
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+let logs = [];
+
+let players = {};
+
+io.on("connection", (socket) => {
+  logs.push(`User connected: ${socket.id}`);
+
+  players[socket.id] = {
+    x: 1290,
+    y: 700,
+  };
+
+  io.emit("updatePlayers", players); //emit to all players
+
+  socket.on("message", (msg) => {
+    logs.push(msg);
+  });
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    const disconnectMsg = `User disconnected: ${socket.id}`;
+    delete players[socket.id];
+    io.emit("updatePlayers", players); //emit to all players
+
+    logs.push(disconnectMsg);
+  });
+});
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/message", (req, res) => {
+  res.json({ message: "Hello from the server!" });
+});
+
+app.get("/", (req, res) => {
+  const logHtml = logs.map((log) => `<p>${log}</p>`).join("");
+  res.send(`
+    <h1>Server Logs</h1>
+    ${logHtml}
+    ${JSON.stringify(players)}
+  `);
+});
+
+server.listen(PORT, () => {
+  const startMsg = `Server is running on http://localhost:${PORT}`;
+  logs.push(startMsg);
+});
---
+++ server/routes/home.js
@@ -0,0 +1,6 @@
+const { Router } = require("express");
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {});
