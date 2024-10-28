ec85a56c9f4bd9a0eba7ae1e1590bbf6f0068c40
harshith <kunderharshith3@gmail.com>
2024-10-29 00:32:54 +0530

    add chats

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const http = require("http");
-
 const app = express();
 const PORT = 5000;
-
 // Create an HTTP server with Express
 const server = http.createServer(app);
 const { Server } = require("socket.io");
@@ -16,33 +14,53 @@ const io = new Server(server, {
 });
 
 let logs = [];
-
 let players = {};
 
 io.on("connection", (socket) => {
-  logs.push(`User connected: ${socket.id}`);
+  console.log(`User connected: ${socket.id}`);
 
-  players[socket.id] = {
-    x: 1290,
-    y: 700,
-  };
+  socket.on("playerJoined", (playerData) => {
+    players[socket.id] = {
+      name: playerData.name,
+      x: playerData.x,
+      y: playerData.y,
+    };
 
-  io.emit("updatePlayers", players); //emit to all players
-
-  socket.on("updatePlayers", (position) => {
-    // Update the player's position
-    players[socket.id] = position;
-
-    // Emit updated player positions to all clients
+    console.log("New player joined:", players[socket.id]);
     io.emit("updatePlayers", players);
   });
 
-  // Handle disconnection
+  socket.on("updatePlayer", (playerData) => {
+    players[socket.id] = {
+      name: playerData.name,
+      x: playerData.x,
+      y: playerData.y,
+    };
+
+    io.emit("updatePlayers", players);
+  });
+
+  socket.on("chatMessage", (data) => {
+    // Find the socket ID of the recipient
+    const recipientSocket = Object.entries(players).find(
+      ([_, player]) => player.name === data.to
+    );
+
+    if (recipientSocket) {
+      // Send message to recipient
+      io.to(recipientSocket[0]).emit("chatMessage", {
+        from: data.from,
+        to: data.to,
+        message: data.message,
+      });
+    }
+  });
   socket.on("disconnect", () => {
     const disconnectMsg = `User disconnected: ${socket.id}`;
-    delete players[socket.id];
-    io.emit("updatePlayers", players); //emit to all players
+    console.log("Player disconnected:", players[socket.id]);
 
+    delete players[socket.id];
+    io.emit("updatePlayers", players);
     logs.push(disconnectMsg);
   });
 });
@@ -56,15 +74,17 @@ app.get("/api/message", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  const logHtml = logs.map((log) => `<p>${log}</p>`).join("");
   res.send(`
     <h1>Server Logs</h1>
-    ${logHtml}
-    ${JSON.stringify(players)}
+    <h2>Connected Players:</h2>
+    <pre>${JSON.stringify(players, null, 2)}</pre>
+    <h2>Server Logs:</h2>
+    ${logs.map((log) => `<p>${log}</p>`).join("")}
   `);
 });
 
 server.listen(PORT, () => {
   const startMsg = `Server is running on http://localhost:${PORT}`;
   logs.push(startMsg);
+  console.log(startMsg);
 });
